a623a029581ec0d628b179f9fd440b95d5cdb114
Luciana Nogueira <o-teu-email@exemplo.com>
2026-08-10 16:42:12 -0300

    fix: restaurar ponto de entrada main.tsx para renderizar o ToonTales

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { VeggieWorldApp } from './presentation/components/VeggieWorldApp';
+import { LocalSecureApp } from './presentation/components/LocalSecureApp';
 import './presentation/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <VeggieWorldApp />
+    <LocalSecureApp />
   </React.StrictMode>
 );
